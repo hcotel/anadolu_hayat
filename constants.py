a5d46c54cb27cc12396b04060458ec2f79c0fddb
hcotel <hcotel@consultant.ffn.com>
2022-05-13 00:17:20 +0300

    add constant

---
+++ constants.py
@@ -0,0 +1,14 @@
+run_optuna = False
+run_city = False
+run_adv = False
+use_nfolds = True
+optuna_trials = 10
+validation_strategy = "adversarial_validation"
+check_val_results = True
+use_scaler = False
+run_imputer = False
+plot_importance = True
+isolation_forest = False
+trial = 35
+validation_files_index = 1
+model_type = "stacking"
